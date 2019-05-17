d223a43af88aa7a76b1fe211f59f284d92862ec8
RogerFK <jorgg69@gmail.com>
2019-05-17 23:24:11 +0200

    First upload
    Took me like 4 hours to make uwu

---
+++ SCP106-Stalk/Events.cs
@@ -0,0 +1,144 @@
+using System.Collections.Generic;
+using System.Linq;
+using Smod2;
+using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using UnityEngine;
+using MEC;
+using ServerMod2.API;
+
+namespace stalky106
+{
+	internal class EventHandlers : IEventHandlerRoundStart, IEventHandlerCallCommand, IEventHandlerSetRole//, IEventHandlerPlayerHurt
+	{
+		// It will ALWAYS ignores spectators and unconnected players.
+		private readonly int[] alwaysIgnore = new int[] { -1, 5, 7 };
+		public readonly string[] defaultRoleNames = new string[] { "0:SCP-173", "1:Class D", "3:SCP-106", "4:NTF Scientist", "5:SCP-049", "6:Scientist",
+			"8:Chaos Insurgent","9:SCP-096", "10:Zombie","11:NTF Lieutenant", "12:NTF Commander", "13:NTF Cadet", "14:Tutorial", "15:Facility Guard",
+			"16:SCP-939-53", "17:SCP-939-89" };
+		private readonly Stalky106 plugin;
+		public EventHandlers(Stalky106 plugin)
+		{
+			this.plugin = plugin;
+		}
+		private int currentCd;
+
+		private IEnumerator<float> PortalAnimation(Scp106PlayerScript auxScp106Component)
+		{
+			Animator anim = auxScp106Component.portalPrefab.GetComponent<Animator>();
+			// I don't know what this does but imma include it so it works flawlessly
+			anim.SetBool("activated", false);
+			yield return 1f;
+			auxScp106Component.portalPrefab.transform.position = auxScp106Component.portalPosition;
+			anim.SetBool("activated", true);
+
+			if (plugin.autoTp) auxScp106Component.CallCmdUsePortal();
+		}
+		private void MovePortalThingy(Scp106PlayerScript auxScp106Component, Vector3 pos)
+		{
+			auxScp106Component.NetworkportalPosition = pos;
+			Timing.RunCoroutine(PortalAnimation(auxScp106Component));
+		}
+		public void OnCallCommand(PlayerCallCommandEvent ev)
+		{
+			if (ev.Command.StartsWith("stalk"))
+			{
+				if (!plugin.enable)
+				{
+					return;
+				}
+				if(ev.Player.TeamRole.Role == Role.SCP_106)
+				{
+					int cdAux = currentCd - PluginManager.Manager.Server.Round.Duration;
+					if (cdAux > 0)
+					{
+						ev.ReturnMessage = plugin.cooldownmsg.Replace("$time", cdAux.ToString());
+						return;
+					}
+					Scp106PlayerScript auxScp106Component = (ev.Player.GetGameObject() as GameObject).GetComponent<Scp106PlayerScript>();
+					if (auxScp106Component != null)
+					{
+						IEnumerable<Player> possibleTargets = PluginManager.Manager.Server.GetPlayers()
+							.Where(p => !plugin.ignoreRoles.Contains((int)p.TeamRole.Role) && !plugin.ignoreTeams.Contains((int)p.TeamRole.Team) && !alwaysIgnore.Contains((int)p.TeamRole.Team));
+						if (possibleTargets.Count() < 1)
+						{
+							ev.ReturnMessage = plugin.notargetsleft;
+							return;
+						}
+						RaycastHit raycastHit;
+						Player victim;
+						do
+						{
+							victim = possibleTargets.ElementAt(Random.Range(0, possibleTargets.Count()));
+							Physics.Raycast(new Ray(victim.GetPosition().ToVector3(), -Vector3.up), out raycastHit, 10f, auxScp106Component.teleportPlacementMask);
+						} while (raycastHit.point.Equals(Vector3.zero));
+						MovePortalThingy(auxScp106Component, raycastHit.point - Vector3.up);
+						currentCd = PluginManager.Manager.Server.Round.Duration + plugin.cooldown;
+						if (plugin.announceReady) Timing.RunCoroutine(AnnounceCooldown(plugin.cooldown));
+						if(plugin.parsedRoleDict.ContainsKey((int)victim.TeamRole.Role))
+						{
+							ev.ReturnMessage = plugin.hauntmessage.Replace("$player", victim.Name).Replace("$class", plugin.parsedRoleDict[(int)victim.TeamRole.Role]);
+						}
+						else
+						{
+							ev.ReturnMessage = plugin.hauntmessage.Replace("$player", victim.Name).Replace("$class", defaultRoleNames[(int)victim.TeamRole.Role]);
+						}
+						ev.Player.PersonalBroadcast(5, ev.ReturnMessage, false);
+					}
+					else
+					{
+						ev.ReturnMessage = plugin.error;
+					}
+				}
+				else
+				{
+					ev.ReturnMessage = plugin.notscp106;
+				}
+			}
+		}
+		public void OnRoundStart(RoundStartEvent ev)
+		{
+			currentCd = plugin.initialCooldown;
+			if(plugin.announceReady) Timing.RunCoroutine(AnnounceCooldown(plugin.initialCooldown));
+		}
+
+		private IEnumerator<float> AnnounceCooldown(int cd)
+		{
+			plugin.Info("Put .stalk on cooldown for " + cd + " seconds.");
+			yield return Timing.WaitForSeconds(cd);
+			plugin.Info("Cooldown for .stalk ended");
+			foreach (Player larry in PluginManager.Manager.Server.GetPlayers(Role.SCP_106))
+			{
+				larry.PersonalBroadcast(8, plugin.stalkready, false);
+			}
+		}
+
+		public void OnSetRole(PlayerSetRoleEvent ev)
+		{
+			if(ev.Role == Role.SCP_106)
+			{
+				ev.Player.PersonalBroadcast(8, plugin.firstbroadcast, false);
+				ev.Player.SendConsoleMessage(plugin.consoleinfo, "white");
+			}
+		}
+
+		/*
+		public void OnPlayerHurt(PlayerHurtEvent ev)
+		{
+			Scp106PlayerScript auxScp106Component = (ev.Player.GetGameObject() as GameObject).GetComponent<Scp106PlayerScript>();
+			if (auxScp106Component.goingViaThePortal && !plugin.attackFromPortal) // attack from portal was a bool in case u're dumbo
+			{
+				ev.Damage = 0;
+				Timing.RunCoroutine(TpBack(ev.Player, ev.Player.GetPosition()));
+			}
+		}
+		private IEnumerator<float> TpBack(Player player, Vector pos)
+		{
+			yield return 0.01f;
+			player.Teleport(pos);
+		}
+		*/
+	}
+}
+
---
+++ SCP106-Stalk/Plugin.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using Smod2;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.Lang;
+
+namespace stalky106
+{
+	[PluginDetails(
+		author = "RogerFK",
+		name = "Stalky 106",
+		description = "Enables a stalk command for SCP-106 to teleport below some random victim, so it's more close to the original lore",
+		id = "rogerfk.scp106stalk",
+		version = "1.0",
+		SmodMajor = 3,
+		SmodMinor = 4,
+		SmodRevision = 0,
+		configPrefix = "stalky",
+		langFile = "stalky_phrases"
+		)]
+
+	public class Stalky106 : Plugin
+	{
+		public override void OnDisable()
+		{
+			this.Info("Larry won't ever stalk you again at night.");
+		}
+
+		public override void OnEnable()
+		{
+			this.Info("Prepare to face Larry...");
+		}
+		public Dictionary<int, string> parsedRoleDict = new Dictionary<int, string>();
+		[ConfigOption]
+		public readonly bool enable = true;
+
+		[ConfigOption]
+		public readonly int cooldown = 30;
+
+		[ConfigOption]
+		public readonly int initialCooldown = 120;
+
+		[ConfigOption]
+		// By default, it ignores SCPs, Chaos Insurgents and the TUTORIAL class
+		public readonly int[] ignoreTeams = new int[] { 0, 2, 6 };
+
+		[ConfigOption]
+		// By default, it ignores SCP-106 and SCP-079
+		public readonly int[] ignoreRoles = new int[] { 3, 7 };
+
+		[ConfigOption]
+		public readonly bool announceReady = true;
+
+		[ConfigOption]
+		public readonly bool autoTp = true;
+
+		[ConfigOption]
+		public readonly string[] roleNames = new string[] { "0:SCP-173", "1:Class D", "3:SCP-106", "4:NTF Scientist", "5:SCP-049", "6:Scientist",
+			"8:Chaos Insurgent","9:SCP-096", "10:Zombie","11:NTF Lieutenant", "12:NTF Commander", "13:NTF Cadet", "14:Tutorial", "15:Facility Guard",
+			"16:SCP-939-53", "17:SCP-939-89" };
+
+		[LangOption]
+		public readonly string firstbroadcast = @"In this server, you can stalk people by putting '.stalk' in the console.\nPress ` to open the console.";
+
+		[LangOption]
+		public readonly string stalkready = @"<color=#0F0>Your '.stalk' command is ready.</color>";
+
+		[LangOption]
+		public readonly string consoleinfo = @"Stalky106 enables additional functionality to SCP-106 by giving him the ability to place a portal to a random player, bringing him closer to the lore. Additionaly, you can use 'cmdbind g .stalk' (for example) to bind it to a key to not have to open the console every time you do it";
+
+		[LangOption]
+		public readonly string hauntmessage = @"<i>You will stalk <b>$player</b>, who is a $class</i>";
+
+		[LangOption]
+		public readonly string notscp106 = "You are not SCP-106!";
+
+		[LangOption]
+		public readonly string notargetsleft = "There's no targets left for you to stalk.";
+
+		[LangOption]
+		public readonly string error = "An error ocurred. Send this command again";
+
+		[LangOption]
+		public readonly string cooldownmsg = "You have to wait $time seconds to use this command";
+
+		public override void Register()
+		{
+			AddEventHandlers(new EventHandlers(this));
+
+			foreach (string key in roleNames)
+			{
+				string[] configInputs = key.Split(':');
+				if (configInputs.Length == 2)
+				{
+					if (!int.TryParse(configInputs[0], out int rolenumber))
+					{
+						Error(configInputs[0] + " isn't a number, when it should (error caught in \'stalky_role_names\')");
+						continue;
+					}
+					if (parsedRoleDict.ContainsKey(rolenumber))
+					{
+						Error("You tried to add " + configInputs[0] + " two times for the \'stalky_role_names\' config option. Just took the first one into account.");
+						continue;
+					}
+					parsedRoleDict.Add(rolenumber, configInputs[1]);
+				}
+				else
+				{
+					this.Error(key + " isn't a valid option for \'stalky_role_names\'.");
+				}
+			}
+			Info("Successfully loaded the configs");
+		}
+	}
+}
