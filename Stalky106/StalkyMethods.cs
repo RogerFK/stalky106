4b264df88b566f0bc034e142f1c3bd45677debd2
Jorge G <jorgg69@gmail.com>
2020-07-20 14:39:39 +0200

    Added consistent name to match gist: https://gist.github.com/RogerFK/b155626b3d987f9c81510e17bcd72c00

--- Stalky106/StalkyMethods.cs
+++ Stalky106/StalkyMethods.cs
@@ -138,7 +138,7 @@ namespace Stalky106
 			}
 
 			;
-			player.Broadcast(6, TokenizedReplacer(plugin.Config.Translations.StalkMessage, '$',
+			player.Broadcast(6, ReplaceAfterToken(plugin.Config.Translations.StalkMessage, '$',
 									new Tuple<string, object>[] { 
 										new Tuple<string, object>("player", target.Nickname),
 										new Tuple<string, object>("class", className), 
@@ -208,8 +208,12 @@ namespace Stalky106
 				}
 			}
 		}
-		private static string TokenizedReplacer(string source, char token, Tuple<string, object>[] valuePairs)
+		public static string ReplaceAfterToken(string source, char token, Tuple<string, object>[] valuePairs)
 		{
+			if (valuePairs == null)
+			{
+				throw new ArgumentNullException("valuePairs");
+			}
 			StringBuilder builder = new StringBuilder(Convert.ToInt32(Math.Ceiling(source.Length * 1.5f)));
 
 			int i = 0;
@@ -217,34 +221,36 @@ namespace Stalky106
 
 			do
 			{
-				// Find the token
+				// Append characters until you find the token
 				char auxChar = token == char.MaxValue ? (char) (char.MaxValue - 1) : char.MaxValue;
 				for (; i < sourceLength && (auxChar = source[i]) != token; i++) builder.Append(auxChar);
 
+				// Ensures no weird stuff regarding token being null
 				if (auxChar == token)
 				{
 					int movePos = 0;
 
+					// Try to find a tuple
 					foreach (Tuple<string, object> kvp in valuePairs)
 					{
 						int j, k;
-						char watchme;
-						for (j = 0, k = i + 1; j < kvp.Item1.Length && k < source.Length && (watchme = source[k]) == kvp.Item1[j]; j++, k++) ;
+						for (j = 0, k = i + 1; j < kvp.Item1.Length && k < source.Length && source[k] == kvp.Item1[j]; j++, k++) ;
+						// General condition for "key found"
 						if (j == kvp.Item1.Length)
 						{
 							movePos = j;
-							builder.Append(kvp.Item2);
+							builder.Append(kvp.Item2); // append what we're replacing the key with
 							break;
 						}
 					}
-					i += movePos;
+					// Don't skip the token if you didn't find the key, append it
+					if (movePos == 0) builder.Append(token);
+					else i += movePos;
 				}
 				i++;
 			} while (i < sourceLength);
 
 			return builder.ToString();
 		}
-
 	}
-
 }
